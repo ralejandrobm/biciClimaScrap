a1a5a7056dbf7294535c836b622a1e152b2c6b99
alexAgnus <ralejandrobm@gmail.com>
2024-04-25 21:48:51 -0600

    quitar warning

--- src/contexts/api/models/DynamicPredictorRequest.py
+++ src/contexts/api/models/DynamicPredictorRequest.py
@@ -18,28 +18,28 @@ class DynamicPredictorRequest(BaseModel):
    
 
     @validator("petal_length")
-    def validate_age(cls, petal_length):
+    def validate_petal_length(cls, petal_length):
         
         if not es_convertible_a_float(petal_length):
             raise ValueError("Invalid petal_length")
         return petal_length
     
     @validator("petal_width")
-    def validate_age(cls, petal_width):
+    def validate_petal_width(cls, petal_width):
         
         if not es_convertible_a_float(petal_width):
             raise ValueError("Invalid petal_width")
         return petal_width
     
     @validator("sepal_length")
-    def validate_age(cls, sepal_length):
+    def validate_sepal_length(cls, sepal_length):
         
         if not es_convertible_a_float(sepal_length):
             raise ValueError("Invalid sepal_length")
         return sepal_length
     
     @validator("sepal_width")
-    def validate_age(cls, sepal_width):
+    def validate_sepal_width(cls, sepal_width):
         
         if not es_convertible_a_float(sepal_width):
             raise ValueError("Invalid sepal_width")
