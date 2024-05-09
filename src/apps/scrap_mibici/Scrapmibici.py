24ecf24bf29f1b3d78a066723c2355ec214132b3
alexAgnus <ralejandrobm@gmail.com>
2024-05-08 18:20:56 -0600

    rutas otra vez

--- src/apps/scrap_mibici/Scrapmibici.py
+++ src/apps/scrap_mibici/Scrapmibici.py
@@ -11,8 +11,8 @@ from pymongo.server_api import ServerApi
 
 class Scrapmibici:
     def __init__(self):
-        self.url_bicis = "app/assets/csv/"
-        self.url_clima = "app/assets/datos_clima/"
+        self.url_bicis = "/app/assets/csv/"
+        self.url_clima = "/app/assets/datos_clima/"
 
     def archivo_info_bicis(self):
         files = os.listdir(self.url_bicis)
